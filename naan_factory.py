230e82619d8c429062184d76e0170ac0fb89e0d7
Shireen Yawer <SYawer@spartaglobal.com>
2019-12-04 15:08:31 +0000

    all tests passing

--- naan_factory.py
+++ naan_factory.py
@@ -6,10 +6,10 @@
 
 def make_dough(arg1, arg2):
     if arg1 != 'water' and arg2 != 'water':
-        return 'not dough'
+        return 'Not dough'
     elif arg1 != 'flour' and arg2 != 'flour':
-        return 'not dough'
-    elif 'water' in [arg1,arg2] and 'flour' in [arg1,arg2]:
+        return 'Not dough'
+    elif 'water' in [arg1, arg2] and 'flour' in [arg1, arg2]:
         return 'dough'
 
 def bake_dough(arg1):
