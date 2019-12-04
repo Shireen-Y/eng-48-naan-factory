c346e62eaf0fbda7033371a0f001abc202143a3c
Shireen Yawer <SYawer@spartaglobal.com>
2019-12-04 15:26:31 +0000

    tests running, code in separate files - all is good

---
+++ naan_factory_run.py
@@ -0,0 +1,12 @@
+from naan_factory_functions import *
+# Here we run the factory (functions)
+
+# We call the functions
+print('Welcome to the Naan Factory :)')
+produce1 = input('What is the first produce? ')
+produce2 = input('What is the second produce? ')
+
+output1 = make_dough(produce1, produce2)
+final_output = bake_dough(output1)
+
+print('Well done! You made:', final_output)
--- naan_factory_tests.py
+++ naan_factory_tests.py
@@ -1,22 +1,6 @@
-# Basis of a test
-# You'll be testing functions or methods, these need to be called or initialised
-# Having controlled inputs for known outputs
-    # And testing for these
+from naan_factory_functions import *
+# Tests go here (for separation of concerns)
 
-
-def make_dough(arg1, arg2):
-    if arg1 != 'water' and arg2 != 'water':
-        return 'Not dough'
-    elif arg1 != 'flour' and arg2 != 'flour':
-        return 'Not dough'
-    elif 'water' in [arg1, arg2] and 'flour' in [arg1, arg2]:
-        return 'dough'
-
-def bake_dough(arg1):
-    if arg1 != 'dough':
-        return 'Not naan'
-    elif arg1 == 'dough':
-        return 'naan'
 
 # Make test for make_dough
 print("testing make_dough with 'water' and 'flour'. Expected --> 'dough'")
